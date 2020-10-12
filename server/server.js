844a038a563b18945f6a50fec1af781f0952f078
PCz <mail.00.kontakt@gmail.com>
2020-10-12 13:13:46 +0200

    change service -> get('*')

--- server/server.js
+++ server/server.js
@@ -4,5 +4,10 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 let app_path = '../docs';
 app.use('/', express.static(path.join(__dirname, app_path)))
-  .get('*',(req,res)=> res.sendFile(path.join((__dirname, app_path +'/index.html'))))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: path.join(__dirname, app_path) })
+})
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// app.get('*',(req,res)=> res.sendFile(path.join((__dirname, app_path +'/index.html'))))
+
